c166b5e594a3ed06327939ef324d09ef6d3a2870
임영우 <hanza7185@gmail.com>
2023-05-10 02:30:44 +0900

    Delete 구구단.js

--- 구구단.js
+++
@@ -1,7 +0,0 @@
-// 구구단 출력하기
-for (let i = 2; i <= 9; i++) {
-    document.write("<h3>" + i + "단</h3>");
-    for (let j = 1; j <= 9; j++) {
-      document.write(i + " x " + j + " = " + i * j + "<br>");
-    }
-  }
